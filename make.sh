a8c2d614b10490701e5ac29b65fa9ccee56a63fe
manuelFabiano <67395603+manuelFabiano@users.noreply.github.com>
2021-07-08 21:58:28 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -6,5 +6,5 @@ gcc -g -c discussioni.c
 gcc -g -c generi.c
 gcc -g -c menu.c
 gcc -g -c main.c
-gcc main.o menu.o discussioni.o recensioni.o utenti.o generi.o -o test
+gcc main.o menu.o discussioni.o recensioni.o utenti.o generi.o -o CineForum
 
