7fa14fd266ebeaf0d807b239426db3173968a5bc
manuelFabiano <67395603+manuelFabiano@users.noreply.github.com>
2021-07-08 21:57:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 #include "discussioni.h"
 #include "generi.h"
 #include "menu.h"
-              
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
               
 int main(){
     lista_utenti utenti= crea_lista_utenti();
